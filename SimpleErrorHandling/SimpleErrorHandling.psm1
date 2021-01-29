0c1bf6333c41483cbaec1cde3e88640bedc26402
Matt Gane <mattpgane@outlook.com>
2021-01-29 15:57:35 +1300

    v1.1 Ready for public release!

--- SimpleErrorHandling/SimpleErrorHandling.psm1
+++ SimpleErrorHandling/SimpleErrorHandling.psm1
@@ -1,8 +1,8 @@
 <#
 .NOTES  
     Author    : Matt Gane
-    Version   : v1.0
-    Released  : 22nd Jan 2021
+    Version   : v1.1
+    Released  : 28th Jan 2021
 #>
 #Dot source all functions in all ps1 files located in the module's public and private folders, excluding tests and profiles.
 Get-ChildItem -Path $PSScriptRoot\public\*.ps1, $PSScriptRoot\private\*.ps1 -Exclude *.tests.ps1, *profile.ps1, *.json -ErrorAction SilentlyContinue |
